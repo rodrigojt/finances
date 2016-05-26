5b6e1aaa214947d6d36fcfed7d2dcaeda43756d1
Rodrigo Teixeira <rodrigo@octocode.com.br>
2016-05-26 19:51:13 +0000

    Criando a página de contas e o arquivo app.html

--- app/app.js
+++ app/app.js
@@ -1,11 +1,11 @@
 import {App, Platform} from 'ionic-angular';
 import {StatusBar} from 'ionic-native';
 import {HomePage} from './pages/home/home';
+import {ContasPage} from './pages/contas/contas';
 
 /* Decorator - Defini a função da classe (App, Page) */
 @App({
-    // Propriedade para identificar a página
-    template: '<ion-nav [root]="rootPage"></ion-nav>',
+    templateUrl: 'build/app.html',
     config: {} // http://ionicframework.com/docs/v2/api/config/Config/
 })
 
